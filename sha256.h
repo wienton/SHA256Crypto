5bac603ec96f63d196575cf375f4a0c3581e0a59
JustCcode <k13775638@gmail.com>
2025-11-26 03:14:54 +0300

    push

--- sha256.h
+++ sha256.h
@@ -1,11 +1,3 @@
-/*********************************************************************
-* Filename:   sha256.h
-* Author:     Brad Conte (brad AT bradconte.com)
-* Copyright:
-* Disclaimer: This code is presented "as is" without any guarantees.
-* Details:    Defines the API for the corresponding SHA1 implementation.
-*********************************************************************/
-
 #ifndef SHA256_H
 #define SHA256_H
 
